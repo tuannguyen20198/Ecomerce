2a7b4f7e29ceb4c2b1b65466f34b5e4600eb3164
tuannguyen20198 <lntt20198@gmail.com>
2023-12-12 21:19:13 +0700

    done access token and refresh token

--- server/routers/user.js
+++ server/routers/user.js
@@ -7,5 +7,7 @@ const ctrls = require("../controllers/user");
 router.post("/register", ctrls.register);
 router.post("/login", ctrls.login);
 router.get("/current", verifyAccessToken, ctrls.getCurrent);
+router.post("/refreshtoken", ctrls.refreshAccessToken);
+router.get("/logout", ctrls.logout);
 
 module.exports = router;
