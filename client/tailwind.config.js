e8800eb5cc67e80a1a2f14b7cb45054ad75ee5bc
tuannguyen20198 <tuannguyen20198@gmail.com>
2024-02-19 21:23:29 +0700

    config taiwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./pblic/idex.html"],
+  content: ["./src/**/*.{html,js}", "./pblic/index.html"],
   theme: {
     fontFamily: {
       main: ["Poppins", "sans-serif;"],
